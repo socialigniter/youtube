d4388c8a3a660c454eb2a345de38334464637f01
Brennan Novak <brennan@reverseproductions.com>
2011-04-20 16:55:01 -0700

    added install file

---
+++ config/install.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:			Social Igniter : YouTuve : Install
+* Author: 		Brennan Novak
+* 		  		contact@social-igniter.com
+*         		@brennannovak
+*          
+* Created: 		Brennan Novak
+*
+* Project:		http://social-igniter.com/
+* Source: 		http://github.com/socialigniter/youtube
+*
+* Description: 	Install values for YouTube App for Social Igniter 
+*/
+/* Settings */
+$config['youtube_settings']['enabled'] 						= 'TRUE';
+$config['youtube_settings']['enabled'] 						= 'TRUE';
+$config['youtube_settings']['consumer_key'] 				= '';
+$config['youtube_settings']['consumer_key_secret']			= '';
+$config['youtube_settings']['social_login']					= 'FALSE';
+$config['youtube_settings']['social_connection']			= 'FALSE';
+$config['youtube_settings']['archive']						= 'TRUE';
+$config['youtube_settings']['default_account']				= '';
+$config['youtube_settings']['recent_videos_display'] 		= '4';
+$config['youtube_settings']['recent_videos_link_actions']	= 'link';
