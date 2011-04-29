14506b4910f3e46bc0587eecf28d9293ba1b116b
Brennan Novak <brennan@reverseproductions.com>
2011-04-29 09:54:45 -0700

    added aarons oauth2 lib

--- controllers/connections.php
+++ controllers/connections.php
@@ -67,4 +67,72 @@ class Connections extends MY_Controller
 */	
 	}	
 	
+	function index()
+	{
+		$redirectURI = base_url().'connections/google';
+
+		//if (get('google_connect_redirect'))
+		//{
+		//	$redirectURI .= '?redirect=' . urlencode(get('google_connect_redirect'));
+		//}
+	
+		$google_config = array(
+			'clientID' 		=> '175060600288.apps.googleusercontent.com',
+			'clientSecret'	=> 'EAxUZv5CKLPAll5FalbfJ3AB',
+			'baseRedirect'	=> $redirectURI
+		);
+	
+		$this->load->library('google_oauth2', $google_config);
+//		$googleAuth = new GoogleAuth('175060600288.apps.googleusercontent.com', 'EAxUZv5CKLPAll5FalbfJ3AB', $redirectURI);
+
+		// The first visit from the mobile app will include an oauth_token from Geoloqi
+		//$this->log_in_from_token();
+
+		if($this->google_oauth2->isCallback())
+		{
+			if($this->google_oauth2->callback())
+			{
+				$response = $this->api->request('connect/google', array(
+					'access_token'  => $this->google_oauth2->accessToken,
+					'refresh_token' => $this->google_oauth2->refreshToken
+				));
+
+	
+				if (property_exists($response, 'result') && $response->result == 'ok')
+				{
+					//$_SESSION['user_profile']->google_email = $response->email;
+				
+					print_r($response);
+				}
+				else
+				{
+					print_r($response);
+					//$this->error(HTTP_SERVER_ERROR, $response->error, k($response, 'error_description'));
+				}
+			
+				/*
+				if(get('redirect'))
+				{
+					redirect(get('redirect'));
+				}
+				else
+				{
+					redirect('/settings/connections');
+				}
+				*/
+				echo 'RIGHT HERE';
+				
+			}
+			else
+			{
+				echo 'HERERERE';
+				//$this->error(HTTP_SERVER_ERROR, 'google_auth_error', 'Unable to authenticate with Google.');
+			}
+		}
+		else
+		{
+			redirect($this->google_oauth2->authorizeURL('http://www.google.com/calendar/feeds/default/allcalendars/full'));
+		}	
+	}
+	
 }
---
+++ libraries/Google_oauth2.php
@@ -0,0 +1,112 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Google_oauth2
+{
+	public $accessToken;
+	public $refreshToken;
+	private $_clientID;
+	private $_clientSecret;
+	private $_baseRedirect;
+
+	public function __construct($google_config)
+	{
+		$this->_clientID	 = $google_config['clientID'];
+		$this->_clientSecret = $google_config['clientSecret'];
+		$this->_baseRedirect = $google_config['baseRedirect'];
+	}
+
+	public function authorizeURL($scope='')
+	{
+		$params = array(
+			'client_id' => $this->_clientID,
+			'response_type' => 'code',
+			'scope' => $scope,
+			'redirect_uri' => $this->_buildRedirectURI()
+		);
+			
+		return 'https://accounts.google.com/o/oauth2/auth?' . http_build_query($params);
+	}
+
+	public function isCallback()
+	{
+		// oauth_callback is set in the redirect URI
+		return array_key_exists('oauth_callback', $_GET);
+	}
+
+	public function callback()
+	{
+		$params = array(
+			'client_id' => $this->_clientID,
+			'redirect_uri' => $this->_buildRedirectURI(),
+			'grant_type' => 'authorization_code',
+			'client_secret' => $this->_clientSecret,
+			'code' => $_GET['code']
+		);
+	
+		$token = $this->_request('https://accounts.google.com/o/oauth2/token', $params);
+		$token = json_decode($token);
+
+		if(is_object($token) && k($token, 'access_token'))
+		{
+			$this->accessToken = $token->access_token;
+			$this->refreshToken = $token->refresh_token;
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+
+	public function _request($url, $post=FALSE)
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		if($post)
+		{
+			curl_setopt($ch, CURLOPT_POST, TRUE);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+		}
+
+		return curl_exec($ch);
+	}
+
+	public function request($url, $params=array(), $post=FALSE)
+	{
+		if($this->accessToken)
+			$params['oauth_token'] = $this->accessToken;
+
+		$params['alt'] = 'jsonc';
+			
+		if($post)
+		{
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_POST, TRUE);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		}
+		else
+		{
+			$url .= '?' . http_build_query($params);
+			$ch = curl_init($url);
+		}
+		
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		$json = curl_exec($ch);
+
+		if($json)
+			return json_decode($json);
+		else
+			return FALSE;
+	}
+	
+	private function _buildRedirectURI()
+	{
+		if(strpos($this->_baseRedirect, '?'))
+			return $this->_baseRedirect . '&oauth_callback';
+		else
+			return $this->_baseRedirect . '?oauth_callback';
+	}
+
+}
